0fba0a7eea4b8f900c0168ac14b813665f399c01
SeanMee86 <sean.mee86@yahoo.com>
2018-11-18 23:51:13 -0800

    lecture 80

--- course-project/src/app/app.component.ts
+++ course-project/src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // recipeDisplay: boolean = true;
+  // shoppingDisplay: boolean = false;
+  loadedFeature = 'Recipes';
 
+  whichDisplay(e) {
+    // if(e === 'Recipes') {
+    //   this.recipeDisplay = true;
+    //   this.shoppingDisplay = false;
+    // }else {
+    //   this.recipeDisplay = false;
+    //   this.shoppingDisplay = true;
+    // }
+    this.loadedFeature = e;
+  }
 }
