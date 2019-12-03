a94ce92e37444dcbef330f8aef7983f8fceec5a4
hss-smee <seanmee8186@gmail.com>
2019-12-03 15:13:07 -0800

    haha

--- course-project/src/app/shopping-list/shopping-list.service.ts
+++ course-project/src/app/shopping-list/shopping-list.service.ts
@@ -8,8 +8,8 @@ export class ShoppingListService {
   ingredientsChanged = new EventEmitter<Ingredient[]>();
 
   private ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatoes', 10)
+    // new Ingredient('Apples', 5),
+    // new Ingredient('Tomatoes', 10)
   ];
 
   getIngredients () {
