e059cd23afae1dfa65dd7bc003e927151ac1deef
hss-smee <seanmee8186@gmail.com>
2020-05-30 22:00:24 -0700

    finished routing section

--- routing-start/src/app/app-routing.module.ts
+++ routing-start/src/app/app-routing.module.ts
@@ -13,6 +13,9 @@ import {ServerComponent} from "./servers/server/server.component";
 import {EditServerComponent} from "./servers/edit-server/edit-server.component";
 import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 import {AuthGuardService} from "./auth-guard.service";
+import {CanDeactivateGuardService} from "./servers/edit-server/can-deactivate-guard.service";
+import {ErrorPageComponent} from "./error-page/error-page.component";
+import {ServerResolverService} from "./servers/server/server-resolver.service";
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -27,16 +30,19 @@ const appRoutes: Routes = [
     canActivateChild: [AuthGuardService],
     component: ServersComponent,
     children: [
-      { path: ':id', component: ServerComponent },
-      { path: ':id/edit', component: EditServerComponent }
+      { path: ':id', component: ServerComponent, resolve: {server: ServerResolverService} },
+      { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuardService] }
     ]
   },
+  // {
+  //   path: 'not-found', component: PageNotFoundComponent
+  // },
   {
-    path: 'not-found', component: PageNotFoundComponent
+    path: 'not-found', component: ErrorPageComponent, data: {message: 'Page Not Found!'}
   },
   {
     path: '**', redirectTo: '/not-found'
-  }
+  },
 ];
 
 @NgModule({
--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import {ActivatedRoute, Params} from "@angular/router";
+import {ActivatedRoute, Params, Router} from "@angular/router";
+import {CanComponentDeactivate} from "./can-deactivate-guard.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
+export class EditServerComponent implements OnInit, CanComponentDeactivate {
   server: {id: number, name: string, status: string};
   serverName = '';
   serverStatus = '';
   allowEdit = false;
+  changesSaved = false;
 
   constructor(private serversService: ServersService,
-              private route: ActivatedRoute) { }
+              private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
     this.route.queryParams
@@ -24,13 +28,33 @@ export class EditServerComponent implements OnInit {
           this.allowEdit = queryParams['allowEdit'] === '1';
         }
       );
+    let id: number = +this.route.snapshot.params.id;
     this.route.fragment.subscribe();
-    this.server = this.serversService.getServer(1);
+    this.server = this.serversService.getServer(id);
+    this.route.queryParams
+      .subscribe(
+        (queryParams: Params) => {
+          id = queryParams['id'];
+        }
+      )
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
   }
 
   onUpdateServer() {
     this.serversService.updateServer(this.server.id, {name: this.serverName, status: this.serverStatus});
+    this.changesSaved = true;
+    this.router.navigate(['../'], {relativeTo: this.route, queryParamsHandling: "preserve"});
+  }
+
+  canDeactivate(): Observable<boolean> | Promise<boolean> | boolean {
+    if (!this.allowEdit) {
+      return true;
+    }
+    if ((this.serverName !== this.server.name || this.serverStatus !== this.server.status) && !this.changesSaved) {
+      return confirm('Do you want to discard the changes?');
+    } else {
+      return true;
+    }
   }
 }
