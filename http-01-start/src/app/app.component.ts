078215493653faa29dd8d330c4eef92e7c480f7a
hss-smee <seanmee8186@gmail.com>
2020-06-07 17:23:56 -0700

    working on http section loading screen

---
+++ http-01-start/src/app/app.component.ts
@@ -0,0 +1,62 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map} from "rxjs/operators";
+import {Post} from "./post.model";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts: Post[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: Post) {
+    // Send Http request
+    this.http
+      .post<{name: string}>(
+        'https://ng-complete-guid-501ad.firebaseio.com/posts.json',
+        postData
+      )
+      .subscribe(
+        (responseData: any) => {
+          console.log(responseData);
+        }
+      );
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http.get<{[key: string]: Post}>('https://ng-complete-guid-501ad.firebaseio.com/posts.json')
+      .pipe(
+        map(responseData => {
+          console.log(responseData);
+          const responseDataArray: Post[] = [];
+          for(const key in responseData) {
+            if(responseData.hasOwnProperty(key)) {
+              responseDataArray.push({ ...responseData[key], id: key})
+            }
+          }
+          return responseDataArray
+      }))
+      .subscribe(
+        (posts) => {
+          this.loadedPosts = posts;
+        }
+      )
+  }
+}
