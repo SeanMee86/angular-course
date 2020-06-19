ab8bbd3c218ecb62b95454fd7b95cf0477c71680
SeanMee86 <sean.mee86@yahoo.com>
2020-06-19 07:02:54 -0700

    updating angular

--- course-project/src/app/app-routing.module.ts
+++ course-project/src/app/app-routing.module.ts
@@ -5,9 +5,9 @@ import {NgModule} from '@angular/core';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/recipes', pathMatch: 'full' },
-  { path: 'recipes', loadChildren: './recipes/recipes/recipes.module#RecipesModule' },
-  { path: 'authenticate', loadChildren: './auth/auth.module#AuthModule'},
-  { path: 'shopping-list', loadChildren: './shopping-list/shopping-list.module#ShoppingListModule'}
+  { path: 'recipes', loadChildren: () => import('./recipes/recipes/recipes.module').then(m => m.RecipesModule) },
+  { path: 'authenticate', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
+  { path: 'shopping-list', loadChildren: () => import('./shopping-list/shopping-list.module').then(m => m.ShoppingListModule)}
 ];
 
 @NgModule({
