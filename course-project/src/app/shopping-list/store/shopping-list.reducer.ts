75f201121340366de7526b1ff196289515f8892f
hss-smee <seanmee8186@gmail.com>
2020-06-20 19:25:01 -0700

    stashing changes

--- course-project/src/app/shopping-list/store/shopping-list.reducer.ts
+++ course-project/src/app/shopping-list/store/shopping-list.reducer.ts
@@ -1,7 +1,7 @@
 import { Ingredient } from '../../shared/ingredient.model';
 import * as ShoppingListActions from './shopping-list.actions';
 
-export interface State {
+interface State {
   ingredients: Ingredient[],
   editedIngredient: Ingredient,
   editedIngredientIndex: number
